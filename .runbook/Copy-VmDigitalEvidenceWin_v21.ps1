97edf7000821837491582d657a02acd425c69668
Fabio Masciotra <fabmas@microsoft.com>
2023-08-30 03:47:48 +0200

    virgolette

--- .runbook/Copy-VmDigitalEvidenceWin_v21.ps1
+++ .runbook/Copy-VmDigitalEvidenceWin_v21.ps1
@@ -456,7 +456,7 @@ if ($bios) {
     Write-Output "########################################################################"
     Write-Output "Operation completed."
     Write-Output "Elapsed time for the entire operation: $($swGlobal.Elapsed.TotalMinutes) minutes"
-    Write-Output 
+    Write-Output ""
     Write-Output "NOTE: $snapshotPrefix is the timestamp prefix used for:  "
     Write-Output "  - the digital evidences stored in the immutable blob container of the SOC Storage Account ($destSAblob)" 
     Write-Output "  - the secrets (hash and BEK) stored in the SOC Key Vault ($destKV)"
